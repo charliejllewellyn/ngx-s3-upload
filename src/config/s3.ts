1b649971a2bb23458b951d81d04f02b24299b71a
charliejllewellyn <charlie.j.llewellyn@gmail.com>
2019-12-01 07:56:54 -0800

    Update s3.ts

--- src/config/s3.ts
+++ src/config/s3.ts
@@ -1,7 +1,7 @@
 export const s3Config = {
     buckets: {
     'ap-south-1': 'uploads.ap-south-1.tensult.com',
-    'eu-west-1': 'cjl-qldb.s3.amazonaws.com'
+    'eu-west-1': 'cjl-qldb'
     },
     defaultRegion: 'eu-west-1'
   };
