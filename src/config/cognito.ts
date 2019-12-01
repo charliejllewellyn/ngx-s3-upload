e883d5bd96b65b81851c15c322d245056a65747c
charliejllewellyn <charlie.j.llewellyn@gmail.com>
2019-12-01 07:21:59 -0800

    Update cognito.ts

--- src/config/cognito.ts
+++ src/config/cognito.ts
@@ -1,7 +1,7 @@
 export let cognitoConfig = {
     userPool: {
         UserPoolId: 'eu-west-1_NqTzvGUTA',
-        ClientId: '1mnp2dbnimseo1vf6hler77a1d',
+        ClientId: '37pa4nvc94bv65pt366ulagsmp',
         region: 'eu-west-1'
     },
     identityPoolId: 'eu-west-1:81da980c-4c2a-46f0-8318-930759e05b5b'
