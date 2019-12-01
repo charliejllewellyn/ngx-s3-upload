54c5615ad1954cca13e679163871793190968446
charliejllewellyn <charlie.j.llewellyn@gmail.com>
2019-12-01 07:12:18 -0800

    Update cognito.ts

--- src/config/cognito.ts
+++ src/config/cognito.ts
@@ -1,8 +1,8 @@
 export let cognitoConfig = {
     userPool: {
-        UserPoolId: 'us-east-1_o07i6UNpK',
-        ClientId: 'a3us4rrhlvcso6eid68c5ue91',
-        region: 'us-east-1'
+        UserPoolId: 'eu-west-1_NqTzvGUTA',
+        ClientId: '1mnp2dbnimseo1vf6hler77a1d',
+        region: 'eu-west-1'
     },
-    identityPoolId: 'us-east-1:cf1c1866-5331-409a-808e-dfa1925bf9fc'
+    identityPoolId: 'eu-west-1:81da980c-4c2a-46f0-8318-930759e05b5b'
 };
